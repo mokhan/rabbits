1baeb524eea0386cf71f1af7dc810db33df3b7a5
mo khan <mo@mokhan.ca>
2014-10-10 10:27:51 -0600

    create a durable queue that will persist messages to disk rather than just memory.

--- lib/example2/send.rb
+++ lib/example2/send.rb
@@ -7,7 +7,7 @@ connection = Bunny.new
 connection.start
 
 channel = connection.create_channel
-queue = channel.queue("hello")
+queue = channel.queue("task_queue", durable: true)
 
 message  = ARGV.empty? ? "Hello World!" : ARGV.join(" ")
 queue.publish(message, persistent: true)
