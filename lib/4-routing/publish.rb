a0cd0ce12644d8826ac3b76b4a8008c7354ca524
mo khan <mo@mokhan.ca>
2014-10-10 11:17:43 -0600

    add example using direct exchange and routing keys.

---
+++ lib/4-routing/publish.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+## encoding: utf-8
+
+require "bunny"
+
+connection = Bunny.new
+connection.start
+
+channel = connection.create_channel
+exchange = channel.direct("direct_logs")
+severity = ARGV.shift || "info"
+message = ARGV.empty? ? "Hello World!" : ARGV.join(" ")
+
+exchange.publish(message, routing_key: severity)
+puts " [x] Sent '#{message}'"
+
+connection.close
